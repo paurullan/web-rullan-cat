a4b8d5b1970d53dbadbf32412707ea694ec3e43e
Pau Ruŀlan Ferragut <paurullan@gmail.com>
2011-10-12 11:33:02 +0200

    Afegida prova de fitxer fabric

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# www.clemesha.org/blog/modern-python-hacker-tools-virtualenv-fabric-pip
+
+__version__ = "0.0.1"
+
+set(
+    fab_users='paurullan',
+    fab_hosts=['kiwi5.apsl.net'],
+    site='web-paurullan',
+    )
+
+def deploy():
+    local('git pull')
+    local('git push')
+    run('cd $(site) && git pull')
+    run('touch reload/paurullan.reload')
+    
